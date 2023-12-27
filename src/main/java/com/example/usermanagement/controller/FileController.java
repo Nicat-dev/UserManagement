0821a8fad287d21e92f976f7018dfe694cf80b2d
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-12-27 22:46:07 +0100

    Create FileController and fixed DataBase Connections

---
+++ src/main/java/com/example/usermanagement/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.example.usermanagement.controller;
+
+import com.example.usermanagement.service.FileStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(name = "/api/v1/file")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileStorageService fileStorageService;
+    @PostMapping("/uploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
+        return new ResponseEntity<>("File " + fileName + " uploaded successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""
+                        + resource.getFilename() + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
+
+}
