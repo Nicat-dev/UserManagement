3bd7160cdf59d238c7adac92509527bc1013542f
Nicat-dev <nijatmehdiyevv@gmail.com>
2024-01-24 23:43:30 +0100

    just delete useless imports

--- src/main/java/com/example/usermanagement/controller/UserController.java
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -4,7 +4,6 @@ import com.example.usermanagement.dto.UserDto;
 import com.example.usermanagement.dto.request.RegisterRequest;
 import com.example.usermanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
